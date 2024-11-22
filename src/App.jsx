d582db88a53e5f8c73e34edc41c01806e22f09d5
pg686 <patritsiya.georgieva@gmail.com>
2024-11-22 20:41:12 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+import './App.css'
+import Button from './Button.jsx'
+
+function App() {
+
+
+  return (
+    <div className='container'>
+     <div className="box">
+      <img src="public/avatar-jessica.jpeg" alt="Daniel" className="image" />
+      <div className='info'>
+        <h2>Jessica Randall</h2>
+        <h5>London, United Kingdom</h5>
+      </div>
+      <h5 className='description'>"Front-end developer and avid reader."</h5>
+      <div className="buttonWrapper">
+      {['GitHub', 'Frontend Mentor', 'LinkedIn', 'Twitter', 'Instagram'].map((content, index) => <Button key={index} content={content} />
+)} 
+      </div>
+     
+     </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button = ({content}) => {
+  return (
+    <div className='button'>{content}</div>
+  )
+}
+
+export default Button
